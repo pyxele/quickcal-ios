f2db030450ef47e12df557d8936080e638f6e17e
Will Boland <will@pyxele.com>
2022-09-11 20:03:08 -0500

    Create PR template and setup Codecov (#5)
    * Create pull_request_template.md
    
    * Add gitignore and add codecov workflow
    
    * Add codecov configuration
    
    * Set codecov workflow to execute on push and pr
    
    * Add base xcode project
    
    * Fix codecov.yml
    
    * Set codecov to use ios 15.5
    
    * Fix ios version
    
    * Change ios to 15.2
    
    * Make iOS version 15.5
    
    * Set to informational
    
    * Add codeowners
    
    * Add swiftlint workflow
    
    * Change job names for codecov and swiftlint

---
+++ QuickCal/QuickCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCalApp.swift
+//  QuickCal
+//
+//  Created by Will Boland on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
