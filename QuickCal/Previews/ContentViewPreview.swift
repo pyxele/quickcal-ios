6fa2785fbd55047ca37bdf2044a248e6084a65b7
Will Boland <will@pyxele.com>
2022-09-11 22:07:40 -0500

    Move preview to seperate folders for codecov

---
+++ QuickCal/Previews/ContentViewPreview.swift
@@ -0,0 +1,14 @@
+//
+//  ContentViewPreview.swift
+//  QuickCal
+//
+//  Created by Will Boland on 9/11/22.
+//
+
+import SwiftUI
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
