20475186ce4bacb3e4ba08fe88cb3085da344f37
Will Boland <will@pyxele.com>
2022-09-12 10:23:49 -0500

    Add delete test case

--- QuickCalUITests/ContentViewUITests.swift
+++ QuickCalUITests/ContentViewUITests.swift
@@ -9,27 +9,35 @@ import XCTest
 
 class ContentViewUITests: XCTestCase {
     let app = XCUIApplication()
+    var startingCellCount = 0
 
     override func setUpWithError() throws {
         continueAfterFailure = false
         app.launch()
+        startingCellCount = app.cells.count
     }
 
     func testContentView() throws {
         let navigation = app.navigationBars.element
-        app.buttons["Add Entry"].tap()
-
         XCTAssert(navigation.exists)
-        XCTAssertGreaterThan(app.cells.count, 0)
     }
 
     func testContentViewAddEntry() throws {
         app.buttons["Add Entry"].tap()
-
-        let navBar = app.navigationBars.element
-        XCTAssert(navBar.exists)
+        XCTAssertGreaterThan(app.cells.count, startingCellCount)
 
         let caloryEntryButton = app.buttons["caloryEntryButton"]
         XCTAssert(caloryEntryButton.exists)
+    }
+
+    func testContentViewDeleteEntry() throws {
+        app.buttons["Add Entry"].tap()
+
+        let cells = app.tables.cells
+        let firstCell = cells.element(boundBy: 0)
+        firstCell.swipeLeft()
+        firstCell.buttons["Delete"].tap()
+
+        XCTAssertEqual(cells.count, startingCellCount)
     }
 }
